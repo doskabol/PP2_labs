8206de589f67f224e989ee2f9114b52b7fcbaad9
Dosbol Abdikarim <a.dosbol06@gmail.com>
2025-02-02 14:32:06 +0500

    VsCode

---
+++ Lab_1/Syntax.py
@@ -0,0 +1,17 @@
+if 5 > 2:
+  print("Five is greater than two!")
+
+#wrong syntax
+if 5 > 2:
+print("Five is greater than two!")
+
+#also wrong
+if 5 > 2:
+ print("Five is greater than two!")
+        print("Five is greater than two!")
+
+#variables
+x = 5
+y = "Hello, World!" 
+
+
