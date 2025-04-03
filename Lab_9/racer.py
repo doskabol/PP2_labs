6cb8cce05d58fdb5807d876a478631cbc2946322
Dosbol Abdikarim <a.dosbol06@gmail.com>
2025-04-03 15:15:21 +0500

    Lab_9

---
+++ Lab_9/racer.py
@@ -0,0 +1,165 @@
+#Imports
+import pygame, sys
+from pygame.locals import *
+import random, time
+
+#Инициализация
+pygame.init()
+
+#Настройка кадров в секунду
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+#Создание цветов
+BLUE  = (0, 0, 255)
+RED   = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+
+#Другие переменные для использования в программе
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 5
+SCORE = 0
+POINT = 0
+#Настройка шрифтов
+font = pygame.font.SysFont("Verdana", 60)
+font_small = pygame.font.SysFont("Verdana", 20)
+game_over = font.render("Game Over", True, BLACK)
+
+background = pygame.image.load(r'AnimatedStreet.png') #загружаем фон
+
+#Создание белого экрана
+DISPLAYSURF = pygame.display.set_mode((400,600))
+DISPLAYSURF.fill(WHITE)
+pygame.display.set_caption("Racer")
+
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load(r'Enemy.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+    def move(self):
+        global SCORE #global позволяет изменять переменную за пределами текущей области видимости
+        self.rect.move_ip(0, SPEED) #Rect.move_ip(x, y) – меняет координаты текущего прямоугольника со смещениями x, y
+        if (self.rect.bottom > 600):
+            SCORE += 1
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+
+class Monet(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load(r'Monet.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (random.randint(40,SCREEN_WIDTH-40), 0)
+    def move(self):
+        global POINT
+        self.rect.move_ip(0,SPEED)
+        if (self.rect.bottom > 600):
+            self.rect.top = 0
+            self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+    def reset(self):
+        self.rect.top = 0
+        self.rect.center = (random.randint(40, SCREEN_WIDTH - 40), 0)
+        global POINT
+        if (POINT+1) % 5 ==0 and POINT!=0:
+            self.image = pygame.image.load(r'Monet.png')
+        else:
+            self.image = pygame.image.load(r'Monet2.png')
+
+        if POINT % 5 ==0 and POINT!=0:
+            POINT+=3
+        else:
+            POINT+=1
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__() 
+        self.image = pygame.image.load(r'Player.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = (160, 520)
+       
+    def move(self):
+        pressed_keys = pygame.key.get_pressed()
+        
+        if self.rect.left > 0:
+              if pressed_keys[K_LEFT]:
+                  self.rect.move_ip(-5, 0)
+        if self.rect.right < SCREEN_WIDTH:        
+              if pressed_keys[K_RIGHT]:
+                  self.rect.move_ip(5, 0) 
+        if self.rect.top > 0 and pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if self.rect.bottom < SCREEN_HEIGHT and pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+                  
+
+        
+P1 = Player()
+E1 = Enemy()
+M1 = Monet()
+
+#создание групп спрайтов
+enemies = pygame.sprite.Group()
+coins = pygame.sprite.Group()
+enemies.add(E1)
+coins.add(M1)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(P1)
+all_sprites.add(E1)
+all_sprites.add(M1)
+
+#новый юзер ивент 
+INC_SPEED = pygame.USEREVENT + 1
+pygame.time.set_timer(INC_SPEED, 1000)
+
+#цикл игры
+while True:
+      
+    for event in pygame.event.get():
+        if event.type == INC_SPEED:
+              SPEED += 0.5      
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+
+    DISPLAYSURF.blit(background, (0,0))
+    scores = font_small.render(str(SCORE), True, BLACK)
+    points = font_small.render(str(POINT), True, RED)
+    DISPLAYSURF.blit(scores, (10,10))
+    DISPLAYSURF.blit(points, (380,10))
+
+    #движение спрайтов
+    for entity in all_sprites:
+        entity.move()
+        DISPLAYSURF.blit(entity.image, entity.rect)
+    
+    #когда проходит сталкновение между игроком и злодеем
+    if pygame.sprite.spritecollideany(P1, enemies):
+          pygame.mixer.Sound(r'lab8_materials_crash.wav').play()
+          time.sleep(1)
+                   
+          DISPLAYSURF.fill(RED)
+          DISPLAYSURF.blit(game_over, (30,250))
+          
+          pygame.display.update()
+          for entity in all_sprites:
+                entity.kill()
+          time.sleep(2)
+          pygame.quit()
+          sys.exit()  
+
+    if pygame.sprite.spritecollideany(E1, coins) :
+        all_sprites.sprites()[2].reset()
+        
+    if pygame.sprite.spritecollideany(P1, coins) : 
+            all_sprites.sprites()[2].reset()
+        
+    pygame.display.update()
+    FramePerSec.tick(FPS)
---
+++ Lab_9/snake.py
@@ -0,0 +1,159 @@
+import pygame
+import time
+import random
+import sys
+
+# начальная скорость змеи
+snake_speed = 10
+
+# размер окна
+window_x, window_y = 800, 600
+
+# пометка цветов
+black, white, red, green, blue, yellow, cyan, purple = (0, 0, 0), (255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255), (160,32,240)
+background_colors = [black, blue, yellow, cyan]
+current_bg_color = black
+
+
+pygame.init()
+
+# настройка окна игры
+pygame.display.set_caption('Snake Game')
+game_window = pygame.display.set_mode((window_x, window_y))
+
+# настройка частоты кадров
+fps = pygame.time.Clock()
+
+# начальные установки змеи
+snake_position = [100, 50]
+snake_body = [[100, 50], [90, 50], [80, 50], [70, 50]]
+
+# размеры еды и их установки
+food_types = {
+    "small": {"color": white, "score": 10},
+    "medium": {"color": purple, "score": 20},
+    "large": {"color": red, "score": 30}
+}
+
+def generate_food():
+    while True:
+        position = [random.randrange(1, window_x // 10) * 10, random.randrange(1, window_y // 10) * 10]
+        if position not in snake_body:
+            food_type = random.choices(list(food_types.keys()), weights=[0.6, 0.3, 0.1])[0]  # Small: 60%, Medium: 30%, Large: 10%
+            return position, food_type
+
+# настройки фруктов
+fruit_position, fruit_type = generate_food()
+fruit_spawn = True
+fruit_timer = time.time() if fruit_type == "large" else None
+
+direction = 'RIGHT'
+change_to = direction
+
+# счетчик уровней и очков
+score = 0
+level = 1
+
+def show_score_and_level(color, font, size): #показывает очки и уровень
+    score_font = pygame.font.SysFont(font, size)
+    score_surface = score_font.render(f'Score: {score}', True, color)
+    level_surface = score_font.render(f'Level: {level}', True, color)
+    game_window.blit(score_surface, score_surface.get_rect(topleft=(10, 10)))
+    game_window.blit(level_surface, level_surface.get_rect(topright=(window_x - 10, 10)))
+
+def game_over():
+    font = pygame.font.SysFont('times new roman', 50)
+    game_over_surface = font.render(f'Your Score: {score}', True, red)
+    game_over_rect = game_over_surface.get_rect(center=(window_x/2, window_y/4))
+    game_window.blit(game_over_surface, game_over_rect)
+    pygame.display.flip()
+    time.sleep(2)
+    pygame.quit()
+    quit()
+
+def check_wall_collision(): #проверяет сталкивается ли змея со стеной
+    if snake_position[0] < 0 or snake_position[0] >= window_x or snake_position[1] < 0 or snake_position[1] >= window_y:
+        game_over()
+
+count = 0
+
+# главный цикл игры
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+            if event.key == pygame.K_UP:
+                change_to = 'UP'
+            elif event.key == pygame.K_DOWN:
+                change_to = 'DOWN'
+            elif event.key == pygame.K_LEFT:
+                change_to = 'LEFT'
+            elif event.key == pygame.K_RIGHT:
+                change_to = 'RIGHT'
+
+    # смена направления
+    if change_to == 'UP' and direction != 'DOWN':
+        direction = 'UP'
+    elif change_to == 'DOWN' and direction != 'UP':
+        direction = 'DOWN'
+    elif change_to == 'LEFT' and direction != 'RIGHT':
+        direction = 'LEFT'
+    elif change_to == 'RIGHT' and direction != 'LEFT':
+        direction = 'RIGHT'
+
+    # движение змеии
+    if direction == 'UP':
+        snake_position[1] -= 10
+    elif direction == 'DOWN':
+        snake_position[1] += 10
+    elif direction == 'LEFT':
+        snake_position[0] -= 10
+    elif direction == 'RIGHT':
+        snake_position[0] += 10
+
+    
+    check_wall_collision()
+
+    # механизм роста змеи
+    snake_body.insert(0, list(snake_position))
+    if snake_position == fruit_position:
+        score += food_types[fruit_type]["score"]
+        fruit_spawn = False
+        
+        # увеличиваем рост и фон каждые 50 очков
+        if (score - count) >= 50:
+            count = score
+            snake_speed += 2
+            current_bg_color = background_colors[(score // 50) % len(background_colors)]
+            level += 1  # Increase level
+        
+        # рост на 2-3 клетки каждые 50 баллов
+        if score < 50:
+            snake_body.append(snake_body[-1])
+        else:
+            for _ in range(random.randint(2, 3)):
+                snake_body.append(snake_body[-1])
+    else:
+        snake_body.pop()
+
+    if not fruit_spawn or (fruit_type == "large" and time.time() - fruit_timer >= 5):
+        fruit_position, fruit_type = generate_food()
+        fruit_spawn = True
+        fruit_timer = time.time() if fruit_type == "large" else None
+
+   
+    game_window.fill(current_bg_color)
+    for pos in snake_body:
+        pygame.draw.rect(game_window, green, pygame.Rect(pos[0], pos[1], 10, 10))
+    pygame.draw.rect(game_window, food_types[fruit_type]["color"], pygame.Rect(fruit_position[0], fruit_position[1], 10, 10))
+
+    # проверка на самосталкновение 
+    if any(block == snake_position for block in snake_body[1:]):
+        game_over()
+
+    # показывать очки и уровень
+    show_score_and_level(white, 'times new roman', 35)
+    pygame.display.update()
+    fps.tick(snake_speed)
